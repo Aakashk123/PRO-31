36c18cfb519493e50c9bd380298ccb40ebe40c50
Aakashk123 <83151262+Aakashk123@users.noreply.github.com>
2021-06-01 21:31:04 +0530

    Add files via upload

---
+++ particle.js
@@ -0,0 +1,9 @@
+class Particles {
+    constructor(x, y, w, h) {
+        var options = {
+
+        thisbody = Bodies.circle(x,y,this.r,options),
+        thiscolor=color(random(0,255),random(0,255),random(0,255)),
+        Worldadd(world,this.body);
+        }
+    }
